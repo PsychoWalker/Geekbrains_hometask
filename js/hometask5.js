c79798ecd4162a97311000605febae8929d2bcf8
PsychoWalker <online-ss4@yandex.ru>
2018-03-15 15:32:51 +0300

    Hometask5

--- js/hometask5.js
+++ js/hometask5.js
@@ -1,12 +1,26 @@
 var chess = document.querySelector('.chess');
+let arrayM = [
+    ['Л','К','С','К','Ф','С','К','Л'],
+    ['П','П','П','П','П','П','П','П'],
+    [],
+    [],
+    [],
+    [],
+    ['П','П','П','П','П','П','П','П'],
+    ['Л','К','С','К','Ф','С','К','Л']
+];
 
 for (var i=0; i < 8; i++) {
     for (var j=0; j<8; j++) {
         var item = document.createElement('div');
         item.classList.add('chess_item');
         chess.appendChild(item);
+
         if (j % 2 !== i % 2) {
             item.classList.add('chess_black');
+        }
+        if( arrayM[i][j] ) {
+            item.innerText = arrayM[i][j];
         }
     }
 }
